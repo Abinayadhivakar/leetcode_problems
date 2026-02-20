5a40d4e8f897435077eb2c42dec31e2f81b1b91f
Abinayadhivakar <abinayaabiaa2238227@gmail.com>
2026-02-20 09:52:26 +0530

    Time: 50 ms (51.23%) | Memory: 17.3 MB (54.71%) - LeetSync

---
+++ 217-contains-duplicate/contains-duplicate.c
@@ -0,0 +1,26 @@
+#include<stdbool.h>
+int compare(const void* a,const void*b)
+{
+  return (*(int*)a-*(int*)b);
+}
+
+bool containsDuplicate(int* nums,int numsize){
+
+    qsort(nums,numsize,sizeof(int),compare);
+
+    for(int i=1;i<numsize;i++)
+    
+        {
+           if(nums[i]==nums[i-1])
+           {
+            return true;
+            
+           }
+           
+        }
+    
+    return false;
+
+    
+
+}
